1078ea2f75b9ad875c52b1c8350771d16205d1c6
Baldaran <kpenev671@gmail.com>
2025-12-25 00:42:18 +0100

    build: finalize Shadow.h umbrella header

--- Shadow.framework/Headers/Shadow.h
+++ Shadow.framework/Headers/Shadow.h
@@ -1,7 +1,13 @@
 #ifndef shadow_h
 #define shadow_h
 
+// The Master Header: Umbrella for the Shadow Framework
+// Importing this single file gives access to the entire stealth engine.
+
 #import <Shadow/Core.h>
 #import <Shadow/Core+Utilities.h>
+#import <Shadow/Ruleset.h>
+#import <Shadow/Settings.h>
+#import <Shadow/Backend.h>
 
 #endif
